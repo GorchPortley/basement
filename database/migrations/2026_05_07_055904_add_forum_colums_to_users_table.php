449afdc46678780c5a73c9d8ec82c3604b21458d
Adrian <admin@sdlabs.cc>
2026-05-08 14:52:57 -0400

    created forum authorization system

---
+++ database/migrations/2026_05_07_055904_add_forum_colums_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('forum_password')->nullable(); #random string in registration hook
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('forum_password');
+        });
+    }
+};
